97da6f4007e2c7b2efc64b14a65ae47f4927a6ac
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-05-23 15:30:34 +0530

    refactor: remove unused file upload API and update FileUploadForm to use direct upload method

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
+import { files } from "./schema";
 
 /**
  * Step 1: Create a SQL client using Neon's serverless driver
