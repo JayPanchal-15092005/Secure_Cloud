7fe7b665869e8b67865da135e4c717adc100db02
Jay_Panchal <trywebsiteapp56@gmail.com>
2026-05-02 15:47:40 +0530

    fix: update .gitignore to exclude desktop.ini, adjust padding in SignUpPage, and remove unused import in db index

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ export default function SignUpPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800">
       <Navbar />
-      <main className="flex-1 flex justify-center items-center p-6">
+      <main className="flex-1 flex justify-center items-center p-6 pt-24">
         <SignUpForm />
       </main>
 
